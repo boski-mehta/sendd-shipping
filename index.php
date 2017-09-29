09c518c6f39c8898f70e6e85e54b292c57a56168
boski-mehta <boskim.3ginfio@gmail.com>
2017-09-29 18:43:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -235,13 +235,13 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 				if(c_country != 'India'){
 					alert(3);
 					 /* live api */
-			 request.open('POST', 'https://api.sendd.co/core/api/v2/order/international/');
+			 /*request.open('POST', 'https://api.sendd.co/core/api/v2/order/international/');
 			request.setRequestHeader('Content-Type', 'application/json');
 			request.setRequestHeader('Authorization', 'Token 0eb688db8076a89861b3885a9cccdcc30edc7a0e'); 
 			request.setRequestHeader('Access-Control-Allow-Origin', '*'); 		
 			/* live api */
 			/* test api */
-			/* request.open('POST', 'https://api-staging.sendd.co/core/api/v2/order/international/');
+			 request.open('POST', 'https://api-staging.sendd.co/core/api/v2/order/international/');
 			request.setRequestHeader('Content-Type', 'application/json');
 			request.setRequestHeader('Authorization', 'Token 39757c4c7867f048ed452812df9f4d7395842de8'); 
 			/* test api */
