8f443fc770a2b9dab10bc07d6964d40c5191dcee
jatinder-pal <boskim.3ginfio@gmail.com>
2017-11-15 17:47:54 +0530

    Update uploadlogo.php

--- uploadlogo.php
+++ uploadlogo.php
@@ -1,8 +1,8 @@
 <?php
 if(is_array($_FILES)) {
-if(is_uploaded_file($_FILES['userImage']['tmp_name'])) {
-$sourcePath = $_FILES['userImage']['tmp_name'];
-$targetPath = "images/".$_FILES['userImage']['name'];
+if(is_uploaded_file($_FILES['fileToUpload']['tmp_name'])) {
+$sourcePath = $_FILES['fileToUpload']['tmp_name'];
+$targetPath = "images/".$_FILES['fileToUpload']['name'];
 if(move_uploaded_file($sourcePath,$targetPath)) {
 ?>
 <img class="image-preview" src="<?php echo $targetPath; ?>" class="upload-preview" />
