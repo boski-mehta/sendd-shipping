570fbbb7e66cea235ea9cf7883e32a93260db6b4
jatinder-pal <boskim.3ginfio@gmail.com>
2017-11-15 15:30:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,8 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 
 
 ?>
+<html>
+<head>
  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,700" rel="stylesheet"> 
  
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
@@ -32,10 +34,58 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"  rel="stylesheet" type="text/css"/>  
 <script src="js/slick.js" type="text/javascript"></script>
  <script src="js/jquery.twbsPagination.js" type="text/javascript"></script>
+<script type="text/javascript" src="js/on-off-switch.js"></script>
+ <link rel="stylesheet" type="text/css" href="css/on-off-switch.css"/>
+ </head>
+ <body>
+ <div class="sidebar">
+<?php include 'dashboard.php';?>
+</div>
+<div class="content">
+<div class="pickupaddress" style="display:none">
+<?php include 'Pickup_address.php';?>	
+</div>
+<input type="hidden" class="access_token_val" value="">
+<?php include 'login-form.php';?>
+<div class="error_msg" style="display:none"><p>Please login to Your Sendd Shipping account First</p></div>
+	<div class="orderform" style="display:none">
 <div class="background_overlay" style="display:none"></div>
 <div class="page"></div>
 <div class="content-container" ></div>
+	</div>
+</div>
 <script>
+$('.page_list li a').click(function(){
+    var show_div=$(this).attr('data-show');
+	$(this).addClass('selected');
+	 $(this).parent().addClass('selected').siblings().removeClass('selected');
+	 
+	 if(show_div =='orderform'){
+	  var shop_url = "<?php echo $_SESSION['shop'];?>";
+				$.post('/checklogin.php', {shop_url:shop_url,getaccesstoken:1}, function(resp){
+					console.log("resp="+resp);
+					//var resp1= resp.find('.session_email').html();
+					resp=$.trim(resp);
+					if(resp!=''){
+						//location.href = 'hiddenpage.php';
+						$('.access_token_val').val(resp);
+						var access_key ='Token '+$('body .access_token_val').val();
+						//alert(access_key);
+						 $('.content').children().hide();
+						$('.orderform').show();		
+					}else{
+					 $('.content').children().hide();
+						$('.error_msg').show();	
+					$('.orderform').hide();						
+					}
+				});
+	 }
+	 else{
+	 $('.content').children().hide();
+	 $('.'+show_div).show();
+	 }
+
+});
 	// Get orders
 	function getorders(page,limit){
          console.log('test122');
@@ -114,11 +164,24 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 		e.preventDefault();
 		$('.popupcontent_inner').remove();
 		$('#popup_content').append('<div class="popupcontent_inner"></div>');
-		var content ;
+		var content,pickup_address='';
 		 var len = $('.select_box:checkbox:checked').length;
 		 if(len > 0){
+			 $.ajax({
+			type: 'POST',
+			async:false,	 
+			url: '/get_pickupaddress.php?pickupaddres=1&shop='+shop,
+			success: function(resp){
+	        	
+				pickup_address=resp;
+				console.log(resp);
+			}
+		});
+				//alert(pickup_address);
 			$('.select_box:checkbox:checked').each(function(index){
 				    var order_id = $(this).val(); 
+					var order_name1 = $(this).parent().next('td').html();
+					var products_name = $(this).attr('data-products_name');
 					var customer_email = $(this).attr('data-customer_email');
 					var customer_name = $(this).attr('data-customer_name');
 					var customer_address = $(this).attr("data-fulladdress");
@@ -129,14 +192,20 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 					var total_weight = $(this).attr('data-total_weight');
 					var quantity_total = $(this).attr('data-quantity_total');
 					var financial_status = $(this).attr('data-financial_status');
-					content ='<div class="item"><div class="item_inner"><h3>Shipping information</h3><input type="hidden" value="'+total_weight+'" data-order_id="'+order_id+'" data-quantity_total="'+quantity_total+'" data-financial_status="'+financial_status+'" class="total_weight"> <div class="fhalf"><h5>Pickup Address*</h5><label>Pickup Company Name:</label><input type="text" class="p_company_name" value="Lomdi Outfitters"><label>Pickup address:</label><select name="pickup_address" class="pickup_address"><option value="4-2-913 Ramkote,Hyderabad,Telangana" selected>Abhishek Enterprises:4-2-913 Ramkote,Hyderabad,Telangana_500001::9885907319</option><option value="D-606, West Vinod Nagar, Behind Ras Vihar Appt" data-address2="IP Extn, Patparganj,East Delhi,Delhi">Lomdi Outfitters:D-606, West Vinod Nagar, Behind Ras Vihar Appt IP Extn, Patparganj, East Delhi,Delhi_110092::9711789367</option></select></div>';
+					content ='<div class="item"><div class="item_inner"><h3>Shipping information</h3><input type="hidden" value="'+total_weight+'" data-order_id="'+order_id+'" data-order_name="'+order_name1+'"  data-quantity_total="'+quantity_total+'" data-products_name= "'+products_name+'" data-financial_status="'+financial_status+'" class="total_weight"> <div class="fhalf">'+pickup_address+'</div>';
 					content = content + '<div class="shalf"><label>Customer Name:</label><input type="text" class="customer_name" value="'+customer_name+'"><br><label>Customer Email:</label><input type="text" class="customer_email" value="'+customer_email+'"><br><label>Customer phone:</label><input type="text" class="customer_phone" value="'+customer_phone+'"><br><label>Customer Address:</label><textarea class="customer_address" value="'+customer_address+'">'+customer_address+'</textarea><label>Total amount pay:</label><input type="text" class="customer_total_price" value="'+customer_total_price+'">';
 					content = content + '<br><label>Payment Type: <p class="payment_method">"'+payment_method+'"</p></label>';
 					content = content + '<br><div class="c_type"><label>Content</label><span><input type="radio" checked value="P" name="content_type" class="content_type">Product</span> <span><input type="radio" value="D" name="content_type" class="content_type">Documents</span></div></div></div>';
+					/*content = content + '<br> <select name="Providers" class="providers"><option value="IP">India Post</option><option value="FE">FedEx</option><option value="BD">BlueDart</option><option value="DH">Delhivery</option><option value="PR">Professional</option><option value="GT">Gati</option><option value="AX">Aramex</option><option value="EE">EcomExpress</option><option value="DT">DotZot</option><option value="FF">First Flight</option><option value="MC">Maruti Courier</option><option value="IP">India Post</option><option value="NE">NuvoEx</option><option value="SF">Shadowfax</option><option value="HL">HL</option></select></div></div>';*/
 
 					$('.popupcontent_inner').append(content);
-		
+		            $('body .p_company_name').val($('body .pickup_address option:selected').attr('data-companyname'));
 			});
+					// set the value of company name
+					$('body .pickup_address').change(function(){
+						 $('body .p_company_name').val($(this).find(':selected').attr('data-companyname'));
+					});
+					// set the value of company name ends
 				var content_last ="<div class='item'><div class='item_inner last'><a href='#' class='Create_order'>Create Order</a></div></div>";
 				$('.popupcontent_inner').append(content_last);
 				// Define data for the popup
@@ -165,25 +234,39 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 
 	
 	$('body').on('click', 'a.Create_order', function(e) {
+	 var access_key ='Token '+$('body .access_token_val').val();
 	$('.response_msg').remove();
 		$( '<div class="load_outer"> <img src="images/loading3.gif" class="loadimg"></div>' ).insertAfter('.Create_order');
 	var leng = $('.popupcontent_inner .item').length;
 	var i=0;
 	$('.popupcontent_inner .item').each(function(index){
 		if(index < leng-1){
-			alert(index +"hello");
+			
 		  /* pickup address detail*/
-		   var pickup_address = $('.pickup_address option:selected',this).text();
-		   var address2 = $('.pickup_address option:selected',this).attr('data-address2');
-		   var pickup_address1 = $('.pickup_address option:selected',this).val();
-		   var content_type = $('.content_type:checked',this).val();
-		   var customer_total_price = $('.customer_total_price',this).val();
+		  var address2 = $('.pickup_address option:selected',this).attr('data-pickupadd2');
+		   var pickup_address1 = $('.pickup_address option:selected',this).attr('data-pickupadd1');
 		   var p_company_name = $('.p_company_name',this).val();
-		   var p_contact_person = pickup_address.split(':')[0];
+		   var p_contact_person = $('.pickup_address option:selected',this).attr('data-pickupusername');
+		   var p_phone = $('.pickup_address option:selected',this).attr('data-pickupphone');
+		   var p_zipcode = $('.pickup_address option:selected',this).attr('data-pickupzip');
+		   var p_city = $('.pickup_address option:selected',this).attr('data-pickupcity');
+		  //var pickup_address = $('.pickup_address option:selected',this).attr('data-pickupusername');
+		  //var pickup_address = $('.pickup_address option:selected',this).text();
+		   //var pickup_address1 = $('.pickup_address option:selected',this).val();
+		  
+		   var content_type = $('.content_type:checked',this).val();
+		   var providers = $('.providers option:selected',this).val();
+		   //alert(providers);
+		   var customer_total_price = $('.customer_total_price',this).val();
+		   
+		   //var p_contact_person = pickup_address.split(':')[0];
+		   
 		   //var p_contact_person = $('.p_contact_person',this).val();
-		   var p_phone = pickup_address.split('::')[1];
+		   //var p_phone = pickup_address.split('::')[1];
+		    
 		   var p_emailid = $('.p_emailid',this).val();
-		   var p_zipcode = pickup_address.split('_')[1].split('::')[0];		   
+		   //var p_zipcode = pickup_address.split('_')[1].split('::')[0];	
+		   
 		   //var p_zipcode = $('.p_zipcode',this).val();
 			//alert("p_zipcode" +p_zipcode);
 		 /* pickup address detail*/
@@ -200,6 +283,12 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 		   }
 		   var total_qty = $('.total_weight',this).attr('data-quantity_total');
 		   var order_id = $('.total_weight',this).attr('data-order_id');
+		   var order_name1 = $('.total_weight',this).attr('data-order_name');
+		   var products_name = $('.total_weight',this).attr('data-products_name');
+		   // set the description limit to 100
+		     if (products_name.length > 100){
+					products_name=products_name.substring(0,100);
+				}
 		   var financial_status = $('.total_weight',this).attr('data-financial_status');
 		   customer_address1 = customer_address.split(',city');
 				
@@ -232,38 +321,20 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 			/* customer detail*/
 	            
 		       	var request = new XMLHttpRequest();
-				if(c_country != 'India'){
-					alert(3);
-					 /* live api */
-			 /*request.open('POST', 'https://api.sendd.co/core/api/v2/order/international/');
-			request.setRequestHeader('Content-Type', 'application/json');
-			request.setRequestHeader('Authorization', 'Token 0eb688db8076a89861b3885a9cccdcc30edc7a0e'); 
-			request.setRequestHeader('Access-Control-Allow-Origin', '*'); 		
-			/* live api */
-			/* test api */
-			 request.open('POST', 'https://api-staging.sendd.co/core/api/v2/order/international/',true);
-			 request.withCredentials = true;
-			request.setRequestHeader('Content-Type', 'application/json');
-			request.setRequestHeader("Access-Control-Allow-Origin", "*");
-			request.setRequestHeader('Authorization', 'Token 39757c4c7867f048ed452812df9f4d7395842de8'); 
-			/* test api */
-					var currency= 'USD';
-					 var international = true;
-					
-				}
-				else{
+				
                /* live api */
-			 /*request.open('POST', 'https://api.sendd.co/core/api/v1/order/');
+			 request.open('POST', 'https://api.sendd.co/core/api/v1/order/');
 			request.setRequestHeader('Content-Type', 'application/json');
-			request.setRequestHeader('Authorization', 'Token 0eb688db8076a89861b3885a9cccdcc30edc7a0e'); 
+			request.setRequestHeader('Authorization', access_key);
 			/* live api */
+			
 			/* test api */
-			request.open('POST', 'https://api-staging.sendd.co/core/api/v1/order/');
+			/*request.open('POST', 'https://api-staging.sendd.co/core/api/v1/order/');
 			request.setRequestHeader('Content-Type', 'application/json');
-			request.setRequestHeader('Authorization', 'Token 97fd62232fa680556005247329f65f5ff0b7eae6');
+			request.setRequestHeader('Authorization', access_key);*/
 			/* test api */
-				}
-             request.onreadystatechange = function () {
+			
+			request.onreadystatechange = function () {
 			  if (this.readyState === 4) {
 				console.log('Status:', this.status);
 				console.log('Headers:', this.getAllResponseHeaders());
@@ -283,13 +354,24 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 								}
 						});
 					 /* add the tracking code in order note */
-					$('.item_inner.last').append("<div class='response_msg'>Order id ="+order_id+" Message = Successfully Shipped</div>");
+					$('.item_inner.last').append("<div class='response_msg'>Order id ="+order_name1+" Message = Successfully Shipped</div>");
 				}
 				else if(json['detail']){
-					$('.item_inner.last').append("<div class='response_msg'>Order id ="+order_id+" Message ="+json['detail']+"</div>");
+					$('.item_inner.last').append("<div class='response_msg'>Order id ="+order_name1+" Message ="+json['detail']+"</div>");
 				}
 				else{
-					$('.item_inner.last').append("<div class='response_msg'>Order id ="+order_id+" Message = Invalid Detail</div>");
+					var error_msg='';
+					$.each(json, function(key, value,){
+							error_msg = error_msg+key+':';
+							$.each(value, function(key, value1){
+							error_msg=error_msg+key+'=';
+							error_msg=error_msg+value1;
+							});
+							error_msg=error_msg+'<br>';
+						});
+						$('.item_inner.last').append("<div class='response_msg'>Order id ="+order_name1+" Message ="+error_msg+"</div>");
+						
+					
 				}
 				
 				if(i == leng-1){
@@ -297,60 +379,12 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 				} 
 			}
 			};
-              if(c_country != 'India'){
-				 var body = { 
-					 "shipments": [{
-					  "item_detail": {
-					   "content": "P",
-					   "currency": "USD",
-					   "purpose": "C",
-					   "weight": ".5",
-					   "sku": "PROSAR0029",
-					   "description": "Bengal Handwoven Cotton Plain Body Ash Black Saree",
-					   "fragile": false,
-					   "value": 112.32,
-					   "qty": 1
-					  }
-					 }],
-					 "process": true,
-					 "customer_reference_id": "#JHAR1395",
-					 "shipping_type": "P",
-					 "email_on_error": ["venkush.khede@jharonka.com"],
-					 "pickup_detail": {
-					  "phone": "+919172667232",
-					  "company_name": "Jharonka",
-					  "address_2": "Gram Panchayat Road, Goregaon East",
-					  "city": "Mumbai",
-					  "country": "IN",
-					  "contact_person": "Gaurav Lad",
-					  "address_type": "O",
-					  "pincode": "400063",
-					  "email": "help@jharonka.com",
-					  "address_1": "B-109 A, Shreyas Industrial Estate,"
-					 },
-					 "insurance": true,
-					 "callback": {
-					  "url": "http://international.craftsvilla.com/archives/order/283/bunch/1"
-					 },
-					 "delivery_detail": {
-					  "phone": "90261170",
-					  "state": "Singapore",
-					  "address_2": "Singapore",
-					  "city": "Singapore",
-					  "country": "SG",
-					  "contact_person": "Bindiya Naulakha",
-					  "pincode": "429074",
-					  "email": "bindiya.n@gmail.com",
-					  "address_1": "03-10, Ola Residences 841, Mountbatten Road,"
-					 },
-					 "cod": false,
-					 "international": true,
-					 "headers": {}
-					}
-				}
-		else{			  
+              
+				 
+				
+					  
 			var body = {
-			  'customer_reference_id':order_id,
+			  'customer_reference_id':order_name1,
 			  'shipments': [
 				{
 				  'item_detail': {
@@ -361,8 +395,8 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 					'weight':0.5,
 					'fragile': false,
 					collectable_value,
-					'description': 'Phone Accessories',
-					currency
+					'description': products_name
+					
 					
 				  }
 				}
@@ -374,6 +408,7 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 				'phone': p_phone,
 				'address_1': pickup_address1,
 				'address_2': address2,
+				'city':p_city,
 				'pincode': p_zipcode,
 				 'country': 'IN',
 				
@@ -388,7 +423,8 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 				'pincode': c_zipcode,
 				'city': c_city,
 				'state': c_state,
-				'country': c_country,
+				'country': c_country
+				//'shipping_company_preference':providers
 				
 			  },
 			  'shipping_type': 'S',
@@ -396,9 +432,8 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 			  'insurance': false,
 			  'process': true,
 			  'notifications':true,
-			  international,
-			};
-          }
+			 };
+          
 			request.send(JSON.stringify(body)); 
 			i++;
 		}
@@ -416,7 +451,8 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 					url: '/trackingcode.php?access_token='+access_token+'&shop='+shop+'&trackingcode='+tracking_no+'&trackingcompany='+tracking_company+'&order_id='+order_id,
 					success: function(data){
 						console.log(data);
-						alert('Tracking Code Added Successfully!');
+						//alert('Tracking Code Added Successfully!');
+						$(this).after('<p style="color:red">Tracking Code Added Successfully!</p>');
 						order_count(); // call order function 
 					}
 				}); 
@@ -436,3 +472,5 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 
 })(jQuery);
 </script>
+</body>
+</html>
