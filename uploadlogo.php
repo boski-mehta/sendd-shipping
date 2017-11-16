fb78fea32c565af05de510d168c4bb9e89f8d65f
jatinder-pal <boskim.3ginfio@gmail.com>
2017-11-16 18:44:50 +0530

    Update uploadlogo.php

--- uploadlogo.php
+++ uploadlogo.php
@@ -6,7 +6,7 @@ $valid_file_formats = array("jpg", "png", "gif", "bmp","jpeg");
 if(is_array($_FILES)) {
 $name = $_FILES['fileToUpload']['name'];
 $size = $_FILES['fileToUpload']['size'];
-echo $shop_url = $_REQUEST['shop_url'];
+$shop_url = $_REQUEST['shop_url'];
 $new_image_name = preg_replace('#^https?://#', '', $shop_url);
 $new_image_name = explode('.', $new_image_name);
 $new_image_name = $new_image_name[0];
@@ -20,18 +20,15 @@ if(file_exists("images/$image_name")) {
 	unlink("images/$image_name");
 }	
 if(move_uploaded_file($tmp, $path.$image_name)){
-	echo "1";
-	 $user_exist = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$shop_url}'");
+	$user_exist = pg_query($dbconn4, "SELECT * FROM user_table WHERE store_url = '{$shop_url}'");
 		if(pg_num_rows($user_exist)){
-			echo "2";
-			$user_exist = pg_query($dbconn4, "UPDATE user_table SET  logo=''$image_name''  WHERE store_url = '{$shop_url}'");
+			$user_exist = pg_query($dbconn4, "UPDATE user_table SET  logo='$image_name'  WHERE store_url = '{$shop_url}'");
 				if($user_exist){
 					echo "Logo uploaded sucessfully";
 					
 				}
 		}
 		else {
-			echo "3";
 			$sql = "insert into user_table (logo) values ('$image_name')";
 			$qry = pg_query($sql);
 			if($qry){
