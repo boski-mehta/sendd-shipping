be420c959393894df06939301a962ccffefcf82b
jatinder-pal <boskim.3ginfio@gmail.com>
2017-11-16 19:09:18 +0530

    Update popupcontent.php

--- popupcontent.php
+++ popupcontent.php
@@ -1,4 +1,4 @@
 <div id="popup_content" style="display:none;">
   <div class="popup_close"><a href="javascript:void(0)" onclick="closepopup();"> Close</a></div>
-
+<div class="popup_close skip_last"><a href="javascript:void(0)" onclick="skip_last();">Skip To Last</a></div>
 </div>
