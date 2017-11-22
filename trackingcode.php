fe09790d90abd5f9c773607b54d4c6c62389e80f
jatinder-pal <boskim.3ginfio@gmail.com>
2017-11-22 18:00:15 +0530

    Update trackingcode.php

--- trackingcode.php
+++ trackingcode.php
@@ -8,10 +8,15 @@ $access_token=$_REQUEST['access_token'];
  $trackingcompany= $_REQUEST['trackingcompany'];
   $products_ids= $_REQUEST['products_ids'];
  print_r($products_ids);
+    $products_ids =explode(',',$products_ids,0);
+foreach($products_ids as $products_ids){
+    $products_ids['id']=$products_ids;
+    }
+//$products_ids = json_encode($products_ids);
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try{
-	$arguments= array( "fulfillment" => array("tracking_number" => $trackingcode,"tracking_company"=> "Custom Tracking Company","tracking_url"=>"http://sendd.co/#/tracking"));
-				
+	//$arguments= array( "fulfillment" => array("tracking_number" => $trackingcode,"tracking_company"=> "Custom Tracking Company","tracking_url"=>"http://sendd.co/#/tracking"));
+	$arguments= array( "fulfillment" => array("tracking_number" => $trackingcode,"tracking_company"=> "Custom Tracking Company","tracking_url"=>"http://sendd.co/#/tracking"),"line_items" => array($products_ids));			
  $orders = $shopify('POST /admin/orders/'.$order_id.'/fulfillments.json',$arguments);
  
 	print_r($orders);
