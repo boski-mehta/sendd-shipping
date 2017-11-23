e5b2f1e12f3a3c33ece77628d7a3fb4ec3cd1b8b
jatinder-pal <boskim.3ginfio@gmail.com>
2017-11-23 18:48:37 +0530

    Update trackingcode.php

--- trackingcode.php
+++ trackingcode.php
@@ -8,27 +8,26 @@ $trackingcode= $_REQUEST['trackingcode'];
 $trackingcompany= $_REQUEST['trackingcompany'];
 $products_ids= $_REQUEST['products_ids'];
 $products_ids =explode(',',$products_ids);
-$i=0;
+
 foreach($products_ids as $key => $products_id){
 	$ids_array[] = array('id' => $products_id);
-	$i++;
 }
 echo "hello";
-$new_array = array_values($ids_array);
 $new_lineitems = array( array ( 'id' => '255841894414'), array ( 'id' => '255841107982'));
+
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try{
-  $arguments	= array( 
-	"fulfillment" => array(
-		"tracking_number" => $trackingcode,
-		"tracking_company"=> "Custom Tracking Company",
-		"tracking_url"=>"http://sendd.co/#/tracking",		
-		"line_items" => $new_lineitems,
-		
-	)	
-); 
-print_r($arguments);
- $orders = $shopify('POST /admin/orders/'.$order_id.'/fulfillments.json',$arguments);
+	$arguments = array(
+		   'fulfillment' => array(
+		   'tracking_number' => $trackingcode,
+		   'tracking_company' => 'Custom Tracking Company',
+		   'tracking_url' => 'http://sendd.co/#/tracking',
+		   'notify_customer' => true,
+		   'line_items' =>  $new_lineitems
+		)
+	);
+	$response = $shopify('POST /admin/orders/' . $order_id . '/fulfillments.json', $arguments);
+	print_r($arguments);
 	print_r($orders);
 }
 catch (shopify\ApiException $e)
