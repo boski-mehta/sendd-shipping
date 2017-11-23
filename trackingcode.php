ecd702c00800c4889ca236fac7fa478a23b26631
jatinder-pal <boskim.3ginfio@gmail.com>
2017-11-23 18:01:08 +0530

    Update trackingcode.php

--- trackingcode.php
+++ trackingcode.php
@@ -3,20 +3,21 @@
 require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 $access_token = $_REQUEST['access_token'];
- $order_id = $_REQUEST['order_id'];
- $trackingcode= $_REQUEST['trackingcode'];
- $trackingcompany= $_REQUEST['trackingcompany'];
-  $products_ids= $_REQUEST['products_ids'];
- print_r($products_ids);
-    $products_ids =explode(',',$products_ids);
-	$i=0;
+$order_id = $_REQUEST['order_id'];
+$trackingcode= $_REQUEST['trackingcode'];
+$trackingcompany= $_REQUEST['trackingcompany'];
+$products_ids= $_REQUEST['products_ids'];
+$products_ids =explode(',',$products_ids);
+$i=0;
+print_r($products_ids);
 foreach($products_ids as $products_id){
     $ids_array[$i]['id']=$products_id;
 	$ids_array[$i]['quantity']=1;
 	$i++;
     }
- $ids_array = json_encode(array($ids_array));
-echo "hello";print_r($ids_array);
+ //$ids_array = json_encode($ids_array);
+echo "hello";
+print_r($ids_array);
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try{
   $arguments	= array( 
