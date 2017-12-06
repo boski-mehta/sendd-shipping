a2b54c2b45ad19101b2b9805ebb8271868d3811d
jatinder-pal <boskim.3ginfio@gmail.com>
2017-12-06 12:13:19 +0530

    Update orders.php

--- orders.php
+++ orders.php
@@ -78,10 +78,10 @@ try
 				}
 				$line_items=$singleorder['line_items'];
 				$count=0;
-				/*foreach($line_items as $line_items){
+				foreach($line_items as $line_items){
 					$count++;
-				}*/
-				//$extra_per_product = $extra_price/$count;
+				}
+				echo $extra_per_product = $extra_price / $count;
 				foreach($line_items as $line_items){
 					//Get product names
 					if($line_items['fulfillment_status']!= 'fulfilled'){
