e7f86c03410eaa778d7d7ea021f7cc06b0a3bb66
jatinder-pal <boskim.3ginfio@gmail.com>
2017-12-06 16:50:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -382,7 +382,7 @@ $('.page_list li a').click(function(){
 			/* live api */
 			
 			/* test api */
-			request.open('POST', 'http://app-staging.sendd.co/core/api/v3/order/');
+			request.open('POST', 'https://api-staging.sendd.co/core/api/v2/order/international/');
 			request.setRequestHeader('Content-Type', 'application/json');
 			request.setRequestHeader('Authorization', access_key);
 			/* test api */
